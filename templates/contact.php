aa0a839807c43e709379a5e027a42f7f6cbcb0d6
reda. <redadahmani34@gmail.com>
2025-02-19 15:27:00 +0100

    fogot what i did just a save

--- templates/contact.php
+++ templates/contact.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style.css">
-  <title>Document</title>
+  <title>Contact</title>
 </head>
 
 <body>
@@ -18,31 +18,34 @@
     <?php include "inc/menu.php" ?>
   </nav>
   <section class="top-page">
-    <h1>Page de Contact</h1>
+    <h1>Pour nous contacter</h1>
     <img src="img/contacts.jpg" alt="">
     <div class="formulaire">
-      <div class="container">
-        <form action="">
+      <form action="">
+        <div class="row">
+          <label for="nom">Nom:</label>
+          <input type="text">
+        </div>
+        <div class="row">
+          <label for="prenom">Prenom:</label>
+          <input type="text">
+        </div>
+        <div class="row">
+          <label for="prenom">Ville:</label>
+          <input type="text">
+        </div>
+        <div class="row">
+          <label for="email">E-MAIL:</label>
+          <input type="text">
+        </div>
+        <div class="row">
+          <label for="commentaire">Commentaire:</label>
+        </div>
+        <textarea name="commentaire" id="comm"></textarea>
 
-          <label for="fname">First Name</label>
-          <input type="text" id="fname" name="firstname" placeholder="Your name..">
-
-          <label for="lname">Last Name</label>
-          <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-
-          <label for="country">Country</label>
-          <select id="country" name="country">
-            <option value="Homme">Homme</option>
-            <option value="canada">Femme</option>
-          </select>
-
-          <label for="subject">Subject</label>
-          <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-
-          <input type="submit" value="Submit">
-
-        </form>
-      </div>
+        <input type="submit" value="Envoyer votre message">
+      </form>
+    </div>
   </section>
 
 </body>
--- templates/liens.php
+++ templates/liens.php
@@ -18,8 +18,34 @@
   </nav>
   <section class="top-page">
     <h1>Page d'accueil</h1>
-    <img src="img/armoiries.png" alt="">
-    <div class="intro">
+    <img src="img/liens.jpg" alt="">
+    <h3>D'autres Capitales se sont associées à notre site.</h3>
+    <div class="links">
+      <div class="link">
+        <a href="">Berlin</a>
+        <a href="">Vienne</a>
+        <a href="">Paris</a>
+        <a href="">Madrid</a>
+        <a href="">Rome</a>
+        <a href="">Londres</a>
+      </div>
+      <div class="link">
+        <a href="">Lisbone</a>
+        <a href="">Prague</a>
+        <a href="">Amsterdam</a>
+        <a href="">Copenhague</a>
+        <a href="">Dublin</a>
+        <a href="">Athènes</a>
+      </div>
+      <div class="link">
+        <a href="">Sofia</a>
+        <a href="">Varsovie</a>
+        <a href="">Heslinki</a>
+        <a href="">Stokholm</a>
+        <a href="">Oslo</a>
+        <a href="">Budapest</a>
+      </div>
+    </div>
 
     </div>
   </section>
