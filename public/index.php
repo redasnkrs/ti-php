a79c92627335f1bf6c174e9d7ce5191f2efd8b9e
reda. <redadahmani34@gmail.com>
2025-02-19 15:36:35 +0100

    added the name of the page with a title

--- public/index.php
+++ public/index.php
@@ -4,27 +4,35 @@
 if (isset($_GET['p'])) {
   switch ($_GET['p']) {
     case "geo":
+      $title = "Géographie";
       include "../templates/geographie.php";
       break;
     case "hist":
+      $title = "Histoire";
       include "../templates/histoire.php";
       break;
     case "cult":
+      $title = "Culture";
       include "../templates/culture.php";
       break;
     case "gal":
+      $title = "Galerie";
       include "../templates/galerie.php";
       break;
     case "form":
+      $title = "Contact";
       include "../templates/contact.php";
       break;
     case "link":
+      $title = "Liens";
       include "../templates/liens.php";
       break;
     default:
+      $title = "ERROR 404";
       include "../templates/page-404.php";
       break;
   }
 } else {
+  $title = "Accueil";
   include "../templates/accueil.php";
 }
